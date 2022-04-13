b146f0d2af779e9d05d38c9a700123b4fb9d9b8b
yasu0604mst <yakubota2020@gmail.com>
2022-04-13 22:48:32 +0900

    update

--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -12,8 +12,7 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <Link href="https://nextjs.org/learn">next.js</Link>
         </p>
         <Link href="/posts/first-post">
             <a>this page!</a>
