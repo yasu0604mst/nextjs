01c33904f76bf450a237d0f8fbfdfa61e3286494
yasu0604mst <yakubota2020@gmail.com>
2022-04-21 13:57:37 +0900

    更新

--- my-app-with-typescript/components/Layout.tsx
+++ my-app-with-typescript/components/Layout.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
-import { Button,Container,Row,Col } from 'react-bootstrap';
+import { Dropdown, Button,Container,Row,Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Common.module.scss'
+import MyHeader from './MyHeader'
+import MyFooter from './MyFooter'
 type Props = {
   children?: ReactNode
   title?: string
@@ -16,43 +18,17 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <Container>
-        <Row>
-          <Col md={1} lg={1}> </Col>
-          <Col md={10} lg={10} xs={12} className="d-flex justify-content-center">
-            <Row className="w-100 d-flex justify-content-center">
-              <Col className={styles.header}><Link className="w-100" href="/">Home</Link></Col>
-              <Col className={styles.header}><Link className="w-100" href="/about">About</Link></Col>
-              <Col className={styles.header}><Link className="w-100" href="/users">UsersList</Link></Col>
-              <Col className={styles.header}><Link className="w-100" href="/api/users">UsersAPIs</Link></Col>
-            </Row> 
-          </Col>
-          <Col md={1} lg={1}> </Col>
-        </Row>
-      </Container>
-    </header>
+    <MyHeader></MyHeader>
     <Container>
-        <Row>
-          <Col md={1} lg={1}> </Col>
-          <Col md={10} lg={10} xs={12} className="">
-          {children}
-          </Col>
-          <Col md={1} lg={1}> </Col>
-        </Row>
-      </Container>
-    <footer>
-      <Container>
-        <Row>
-          <Col md={1} lg={1}> </Col>
-          <Col md={10} lg={10} xs={12} className="d-flex justify-content-center">
-            <span>company copy light)</span>
-          </Col>
-          <Col md={1} lg={1}> </Col>
-        </Row>
-      </Container>
-    </footer>
+      <Row>
+        <Col md={1} lg={1}> </Col>
+        <Col md={10} lg={10} xs={12} className="">
+        {children}
+        </Col>
+        <Col md={1} lg={1}> </Col>
+      </Row>
+    </Container>
+    <MyFooter></MyFooter>
   </div>
 )
-
 export default Layout
