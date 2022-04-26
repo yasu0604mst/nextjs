42a2e6d99a5ec255b65af5c949aa34da34cd69ea
yasu0604mst <yakubota2020@gmail.com>
2022-04-27 01:41:25 +0900

    更新

--- my-app-with-typescript/components/MyCarousel.tsx
+++ my-app-with-typescript/components/MyCarousel.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ImageCarousel from './ImageCarousel';
+import styles from '../styles/Common.module.scss'
 
 const MyCarousel = () => (
 <Carousel className='my-2'>
@@ -11,8 +12,8 @@ const MyCarousel = () => (
       alt="First slide"
     />
     <Carousel.Caption>
-        <h3 className='mb-0' style={{background:'#0002'}}>ページに映える写真を撮影</h3>
-        <p style={{background:'#0002'}}>写真はSNSのアイコンなどに使用することを想定し、明るく映えるような写真を撮影します。</p>
+        <h3 className={styles.carousel_h3} style={{background:'#0003'}}>ページに映える写真を撮影します</h3>
+        <p className={styles.carousel_p} style={{background:'#0003'}}>写真はSNSのアイコンなどに使用することを想定し、明るく映えるような写真を撮影します。</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -23,8 +24,8 @@ const MyCarousel = () => (
     />
 
     <Carousel.Caption>
-        <h3 className='mb-0' style={{background:'#0002'}}>自然な写真を撮影</h3>
-        <p style={{background:'#0002'}}>友達に撮られたかのような自然な写真撮影を心がけています。</p>
+        <h3 className={styles.carousel_h3} style={{background:'#0003'}}>自然な写真を撮影します</h3>
+        <p className={styles.carousel_p} style={{background:'#0003'}}>友達に撮られたかのような自然な写真撮影を心がけています。</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -35,8 +36,8 @@ const MyCarousel = () => (
     />
 
     <Carousel.Caption >
-        <h3 className='mb-0' style={{background:'#0002'}}>卒業式、卒園式、入学式の記念撮影にも</h3>
-        <p style={{background:'#0002'}}>思い出の1日を写真にします</p>
+        <h3 className={styles.carousel_h3} style={{background:'#0003'}}>卒業式、入学式の記念撮影にも</h3>
+        <p className={styles.carousel_p} style={{background:'#0003'}}>思い出の1日を写真にします</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
