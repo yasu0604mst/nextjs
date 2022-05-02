89d541c403e02b5ac6b1b33cb8fe43b26fcfa87b
yasu0604mst <yakubota2020@gmail.com>
2022-05-02 18:34:22 +0900

    更新

--- my-app-with-typescript/components/Layout.tsx
+++ my-app-with-typescript/components/Layout.tsx
@@ -21,8 +21,8 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-6F912BMM71"></script>
     </Head>
     <MyHeader></MyHeader>
-    <Container fluid>
-      <Row>
+    <Container fluid className='' style={{margin:'60px 0 0 0'}}>
+      <Row >
         <Col md={1} lg={1}> </Col>
         <Col md={10} lg={10} xs={12} className="">
         {children}
